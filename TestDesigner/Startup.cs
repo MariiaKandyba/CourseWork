edc7b75aa34f9862810ff236e92a63dd3daffcdb
Admin <Admin@DESKTOP-HEOO8JS>
2023-10-25 18:13:45 +0300

    services applied

---
+++ TestDesigner/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDesigner.Services;
+using TestDesigner.Services.TestDesigner.Services;
+
+namespace TestDesigner
+{
+    public class Startup
+    {
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<MainWindow>();
+            services.AddTransient<IFileService, FileService>();
+            services.AddTransient<ITestService, TestService>();
+            services.AddTransient<ISerializationService, SerializationService>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+
+}
