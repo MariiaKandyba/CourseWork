e661a1d3f0f99244f99bd708129e650e8f127734
Admin <Admin@DESKTOP-HEOO8JS>
2023-10-26 13:21:23 +0300

    folder with entity

---
+++ DALTest/Entities/UserAnswer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALTest.Entities
+{
+    public class UserAnswer
+    {
+        [Key]
+        public int Id { get; set; }
+        public bool IsChecked { get; set; }
+
+        public int UserTestId { get; set; }
+        public virtual UserTest UserTest { get; set; }
+
+        public int AnswerId { get; set; }
+        public virtual Answer Answer { get; set; }
+    }
+
+}
